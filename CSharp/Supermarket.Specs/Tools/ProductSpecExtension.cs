fde941ac30f7317f505ee20c4053508c30e7c6ac
Paul Rohorzka <paul@paulroho.com>
2016-10-31 15:34:51 +0100

    Fixed generation of fake EAN codes

--- CSharp/Supermarket.Specs/Tools/ProductSpecExtension.cs
+++ CSharp/Supermarket.Specs/Tools/ProductSpecExtension.cs
@@ -4,16 +4,20 @@ namespace Supermarket.Specs.Tools
 {
     public static class ProductSpecExtension
     {
+        private static int _modulusEan9 = (int)1E9;
+
         public static EAN GetEANFromHash(this Product product)
         {
-            var hash = GetAllDigitHash(product.Name, 9);
+            var hash = GetAllDigitHash(product.Name, 9, _modulusEan9);
             return EAN.NewEAN(hash);
         }
 
-        private static string GetAllDigitHash(string text, int numDigits)
+        private static string GetAllDigitHash(string text, int numDigits, int modulus)
         {
-            var shorthash = text.GetHashCode()%10^numDigits;
-            if (shorthash < 0) shorthash *= -1;
+            var shorthash = text.GetHashCode();
+            if (shorthash < 0)
+                shorthash *= -1;
+            shorthash = shorthash % modulus;
             return shorthash.ToString(new string('0', numDigits));
         }
     }
